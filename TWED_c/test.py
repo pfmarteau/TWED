23dae52f8fa544f054641d0f9249cbd6c4b37e41
Pierre-François Marteau <pierrefrancois.marteau@gmail.com>
2020-10-14 17:48:09 +0200

    Add files via upload

---
+++ TWED_c/test.py
@@ -0,0 +1,22 @@
+import numpy as np
+import TWED
+
+if __name__ == '__main__':
+    import matplotlib.pyplot as plt
+    A=np.array([[0],[0],[1],[1],[2],[3],[5],[2],[0],[1],[-0.1]])
+    tA=np.array(list(i for i in range(len(A))))
+    B=np.array([[0],[1],[2],[2.5],[3],[3.5],[4],[4.5],[5.5],[2],[0],[0],[.25],[.05],[0]])
+    tB=np.array(list(i for i in range(len(B))))
+    C=np.array([[4],[4],[3],[3],[3],[3],[2],[5],[2],[.5],[.5],[.5]])
+    tC=np.array(list(i for i in range(len(C))))
+    nu=.1
+    _lambda=.2
+    print("twed(A,B,nu,lambda)=", TWED.distance(A,tA,B,tB,nu,_lambda))
+    print("twed(A,C,nu,lambda)=", TWED.distance(A,tA,C,tC,nu,_lambda))
+    print("twed(B,C,nu,lambda)=", TWED.distance(B,tB,C,tC,nu,_lambda))
+        
+    plt.plot(A, label='A')
+    plt.plot(B, label='B')
+    plt.plot(C, label='C')
+    plt.legend()
+    plt.show()
