549c66d2216353520aa274104296b938ea2c4537
Pierre-François Marteau <pierrefrancois.marteau@gmail.com>
2025-10-29 09:42:11 +0100

    Add include_dirs for numpy in setup.py

--- TWED_c/setup.py
+++ TWED_c/setup.py
@@ -1,8 +1,12 @@
 from distutils.core import setup, Extension
 import numpy
 # define the extension module
-TWED = Extension('TWED', sources=['TWED.c'])
+TWED = Extension(
+    'TWED', sources=['TWED.c'],
+    include_dirs=[numpy.get_include()],
+    )
 
 # run the setup
 setup(ext_modules=[TWED])
+
 
